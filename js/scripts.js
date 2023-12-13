cfde96bb19832b3687b4cd5ef2de3d3713e3812e
shinannkaku <shinannkaku@gmail.com>
2023-12-13 23:23:13 +0800

    1213 模擬廣告輪轉器&頁面修改
    1213 模擬廣告輪轉器&頁面修改

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,35 @@
-/*!
-* Start Bootstrap - Small Business v5.0.6 (https://startbootstrap.com/template/small-business)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-small-business/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+let slideIndex = 0;
+carousel();
 
+function carousel() {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(carousel, 2000); // 切换图片的间隔时间（以毫秒为单位）
+}
 
-    
+// 手动切换图片
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  const slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
