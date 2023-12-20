eb6ebaae62007e6bde92044beb44c9f0575abb6f
jaintzu <a110340019@mail.shu.edu.tw>
2023-12-20 22:51:30 +0800

    修改整個登入後的畫面，還有加薪的兩個部落格的留言功能

---
+++ js/blog-lol.js
@@ -0,0 +1,10 @@
+var url = "https://sheets.googleapis.com/v4/spreadsheets/1Hd9tQOUIkcPFLgo2BkzC61qKKF5xZ_wppxJxJIIMKbc/values/Sheet1?alt=json&key=AIzaSyB1ja51HOOIp0wgT-3nEz2P50zkb_zUASQ";
+//const form = document.forms['userForm'];
+$.getJSON(url, function(json) {
+    var values = json.values;// 所有試算表資料
+    var text = document.querySelector('.htmltext');
+    //將留言顯示在網頁上
+    values.slice(1).forEach((value) =>{
+        text.innerHTML += "<div class='d-flex mb-4'><div class='flex-shrink-0'><img class='rounded-circle' src='https://dummyimage.com/50x50/ced4da/6c757d.jpg' alt='...' /></div><div class='ms-3'><div class='fw-bold'>使用者</div>" + value + "</div>";
+    });
+});
