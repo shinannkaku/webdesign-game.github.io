3ddd8036f01a1db4d4b207f794355bd5a5942712
jaintzu <a110340019@mail.shu.edu.tw>
2023-12-22 23:35:07 +0800

    增加留言輸入暱稱功能、如未輸入則自動替換為匿名使用者

--- js/blog-rhythmhive.js
+++ js/blog-rhythmhive.js
@@ -5,6 +5,6 @@ $.getJSON(url, function(json) {
     var text = document.querySelector('.htmltext');
     //將留言顯示在網頁上
     values.slice(1).forEach((value) =>{
-        text.innerHTML += "<div class='d-flex mb-4'><div class='flex-shrink-0'><img class='rounded-circle' src='https://dummyimage.com/50x50/ced4da/6c757d.jpg' alt='...' /></div><div class='ms-3'><div class='fw-bold'>使用者</div>" + value + "</div>";
+        text.innerHTML += "<div class='d-flex mb-4'><div class='flex-shrink-0'><img class='rounded-circle' src='https://dummyimage.com/50x50/ced4da/6c757d.jpg' alt='...' /></div><div class='ms-3'><div class='fw-bold'>" + value[0] + "</div>" + value[1] + "</div>";
     });
 });
