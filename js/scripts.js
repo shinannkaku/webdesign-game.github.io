9929be8116fc1e7d0ee131427e76bcf45e682e52
shinannkaku <shinannkaku@gmail.com>
2023-12-28 15:58:39 +0800

    1228把錯誤的程式碼刪掉
    my_style.css/js

--- js/scripts.js
+++ js/scripts.js
@@ -34,15 +34,3 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
 }
 
-const keys = document.querySelectorAll('.piano-key');
-
-// 添加點擊事件或其他互動效果
-keys.forEach(key => {
-  key.addEventListener('click', () => {
-    // 在這裡可以添加音樂播放或其他功能
-    // 例如：播放對應音符的聲音
-    // 可以使用 HTML5 的 Audio 元素或其他音樂播放 API
-    // 這裡只是個範例，你可能需要加入更多的功能
-    console.log('音符被點擊了');
-  });
-});
